03c7ca57c85b879f966b609695beb4fe415a845c
DDonghyeo <gsu9045000@gmail.com>
2024-01-08 17:04:00 +0900

    feat: Init

---
+++ src/main/java/com/kim3ho1/yourprotein/YourproteinApplication.java
@@ -0,0 +1,13 @@
+package com.kim3ho1.yourprotein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourproteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourproteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kim3ho1/yourprotein/YourproteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim3ho1.yourprotein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourproteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
