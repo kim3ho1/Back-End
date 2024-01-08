8285dfc7dd706d8a48bd29e2d5cd655f6625edcc
DDonghyeo <gsu9045000@gmail.com>
2024-01-08 17:39:24 +0900

    feat: User Domain

---
+++ src/main/java/com/kim3ho1/yourprotein/domain/User.java
@@ -0,0 +1,47 @@
+package com.kim3ho1.yourprotein.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
+@Entity
+@Table(name = "user")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String profileImage;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Long kakaoId;
+
+}
