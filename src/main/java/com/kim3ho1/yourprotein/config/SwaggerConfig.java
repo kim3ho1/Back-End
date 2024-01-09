2ce9dae89d0549479bda7144427319d27e46789c
DDonghyeo <gsu9045000@gmail.com>
2024-01-09 19:06:16 +0900

    feat: Swagger Configuration'

---
+++ src/main/java/com/kim3ho1/yourprotein/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.kim3ho1.yourprotein.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+    // url : http://localhost:8000/swagger-ui/index.html#/
+    @Bean
+    public OpenAPI getOpenApi() {
+        Server server = new Server().url("/");
+
+        return new OpenAPI()
+                .info(getSwaggerInfo())
+                .components(authSetting())
+                .addServersItem(server);
+    }
+
+    private Info getSwaggerInfo() {
+        License license = new License();
+        license.setName("{Application}");
+
+        return new Info()
+                .title("Spon-Us API Document")
+                .description("Spon-Us의 API 문서 입니다.")
+                .version("v0.0.1")
+                .license(license);
+    }
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "access-token",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"))
+                .addSecuritySchemes(
+                        "refresh-token",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("refreshToken"));
+    }
+}
+
