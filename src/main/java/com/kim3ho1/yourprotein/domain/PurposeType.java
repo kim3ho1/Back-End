6718313b888abb1da481ab2d742d313615ab14a6
seheonnn <ho78901@naver.com>
2024-01-10 23:24:40 +0900

    Refactor: Enum 명 수정

--- src/main/java/com/kim3ho1/yourprotein/domain/PurposeType.java
+++ src/main/java/com/kim3ho1/yourprotein/domain/PurposeType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PurposeType {
 	NORMAL(1,0.8, "NORMAL"), // 운동 안 하는 사람, 콩판 질환 있는 사람
-	WEIGHT(2,1.2, "WEIGHT"), // 운동 하는 사람
+	TRAINING(2,1.2, "TRAINING"), // 운동 하는 사람
 	BULK(3,2.0, "BULK"); // 벌크업을 원하는 사람
 	private final int index;
 	private final Double value;
